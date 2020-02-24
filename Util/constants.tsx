42ba1d254de0ee2568aba5da150a13a87a497ea4
AndrewLBeasley <alloydbeasley@gmail.com>
2020-02-23 18:05:21 -0600

    whigepaper constants, iframes, testing react-pdf

---
+++ Util/constants.tsx
@@ -0,0 +1,3 @@
+export const ETH_WHITEPAPER: string = "https://s3.amazonaws.com/eth.proto.map/Ethereum_white_paper-a_next_generation_smart_contract_and_decentralized_application_platform-vitalik-buterin.pdf";
+
+export const MAKER_WHITEPAPER: string = "https://s3.amazonaws.com/maker.proto.map/White+Paper+-The+Maker+Protocol_+MakerDAO%E2%80%99s+Multi-Collateral+Dai+(MCD)+System-FINAL-+021720.pdf";
--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,28 @@
 import * as React from "react";
-import "./Styles/StyleMain.css"
+import { Document } from 'react-pdf';
+import { ETH_WHITEPAPER, MAKER_WHITEPAPER } from "../../Util/constants";
 
-const App  = () => {
-    return(
-        <div className="app" id="app"></div>
+
+
+import "./Styles/StyleMain.css";
+
+const App = () => {
+    return (
+        <div className="app" id="app">
+            <div>protomap</div>
+            <iframe
+                className="ether-whitepaper whitepaper"
+                src={ETH_WHITEPAPER}
+                frameBorder="0">
+            </iframe>
+            <iframe
+                className="maker-whitepaper whitepaper"
+                src={MAKER_WHITEPAPER}
+                frameBorder="0">
+            </iframe>
+
+            <Document file={ETH_WHITEPAPER}></Document>
+        </div>
     );
 };
 
